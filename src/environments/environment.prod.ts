2f587f77e402c0f5ffdbf50ced1dde3a2adda556
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-08 04:56:09 -0500

    fix background image tmp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   api: 'https://sofka-rojas.herokuapp.com/api',
-  img: 'https://jesus-rojas.github.io'
+  img: 'https://jesus-rojas.github.io/sofka-frontend'
 };
